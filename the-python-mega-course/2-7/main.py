f348ed84b29a585ea4af4f56a767e483665e47b8
gsteelman <gsteelman@twitter.com>
2017-01-18 20:49:21 -0800

    Lecture 2-7

---
+++ the-python-mega-course/2-7/main.py
@@ -0,0 +1,14 @@
+print("Hello world")
+replaced = "Foo".replace("F", "B")
+print(replaced)
+replaced = "Bar".replace("Ba", "He")
+print(replaced)
+replaced = "Amazing".replace("zing", "ze")
+print(replaced)
+number = 1
+print(number)
+number = 1.5
+print(number)
+var_with_underscores = "idk"
+print(var_with_underscores)
+print(replaced, number, var_with_underscores)
