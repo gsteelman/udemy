bc046558fafd4fd83cacc8fbbbcaa799254932cd
gsteelman <gsteelman@twitter.com>
2017-01-22 18:04:43 -0800

    Lecture 3-16.

---
+++ the-python-mega-course/3-16/main.py
@@ -0,0 +1,19 @@
+print("asdf")
+print('asdf')
+print(type("asdf"))
+print(type('asdf'))
+
+s = "asdf"
+print(s)
+print(type(s))
+
+print("7" + "1")
+print(int("7") + 1)
+
+pString = "python is cool"
+print(pString[10])
+print(pString[10:])
+print(pString[:10])
+print(pString[10:14])
+print(pString[-4:])
+print(pString * 2)
