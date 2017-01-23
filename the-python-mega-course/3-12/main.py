179a068b2124fcf06b8bffb32809d2afc321471c
gsteelman <gsteelman@twitter.com>
2017-01-22 17:40:46 -0800

    Lecture 3-12.

---
+++ the-python-mega-course/3-12/main.py
@@ -0,0 +1,19 @@
+import math
+
+print(4 + 5)
+print(4.0 + 5)
+print(0 - .5)
+print(7 * 2)
+print(7 * 2.5)
+print(7 / 3)
+print(7 / 3.0)
+print(7 % 3)
+print(7 % 2.5)
+print(3 ** 2)
+print(3.0 ** 2)
+print(3 ** 2.5)
+print(math.sqrt(9))
+# division has priority over multiplication
+print(1 + 3 * 10 / 5)
+# exponentiation has priority over others
+print(1 + 3 * 10 / 5 ** 2)
