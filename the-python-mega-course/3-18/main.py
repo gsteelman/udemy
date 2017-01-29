4a840ef788cb2811d50a466a18f83c392f881dec
gsteelman <gsteelman@twitter.com>
2017-01-28 19:18:36 -0800

    Lesson 3-18.

---
+++ the-python-mega-course/3-18/main.py
@@ -0,0 +1,10 @@
+myDict = {"name" : "gary", "profession" : "superware engineer"}
+print(myDict["name"])
+
+myDictValues = myDict.values()
+myDictKeys = myDict.keys()
+
+print(myDictValues)
+print(myDictKeys)
+print(type(myDictValues))
+print(type(myDictKeys))
